7bf6e12eff35a27491197733685a50583a3766c2
Feynman-young <yangw.ing@foxmail.com>
2025-02-27 17:55:13 +0800

    socket api

--- net/src/socket.c
+++ net/src/socket.c
@@ -1,8 +1,14 @@
 // Basic linux network programming api
 
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
 
 // Big endian and little endian
 void byteorder() {
@@ -43,8 +49,89 @@ void test_hton_ntohl() {
     printf("\n");
 }
 
+#define PORT_ 54321
+#define M_CONN_ 5
+#define S_ADDR_ "127.0.0.1"
+
+void server() {
+
+    struct sockaddr_in s_addr;
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_addr.s_addr = INADDR_ANY;
+    s_addr.sin_port = htons(PORT_);
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket() error");
+        exit(1);
+    }
+
+    if (bind(server_fd, (struct sockaddr *) &s_addr, sizeof(s_addr)) < 0) {
+        perror("bind() error");
+        exit(1);
+    }
+
+    if (listen(server_fd, M_CONN_) < 0) {
+        perror("listen() error");
+        exit(1);
+    }
+
+    struct sockaddr_in c_addr;
+    socklen_t c_addrlen = sizeof(c_addr);
+
+    int client_fd = accept(server_fd, (struct sockaddr *)&c_addr, &c_addrlen);
+    if (client_fd < 0) {
+        perror("accept() error");
+        exit(1);
+    }
+
+    char remote[INET_ADDRSTRLEN];
+    printf("connect with %s:%d\n",
+        inet_ntop(AF_INET, &c_addr.sin_addr, remote, INET_ADDRSTRLEN),
+        ntohs(c_addr.sin_port)
+    );
+    close(client_fd);
+    close(server_fd);
+}
+
+void client() {
+
+    struct sockaddr_in s_addr;
+    s_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, S_ADDR_, &s_addr.sin_addr);
+    s_addr.sin_port = htons(PORT_);
+
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd < 0) {
+        perror("socket() error");
+        exit(1);
+    }
+
+    if (connect(client_fd, (struct sockaddr *) &s_addr, sizeof(s_addr)) < 0) {
+        perror("connect() error");
+        exit(1);
+    }
+
+    close(client_fd);
+}
+
+void conn() {
+    pid_t pid;
+    pid = fork();
+
+    if (pid == 0) {
+        sleep(1);
+        client();
+        exit(0);
+    } else {
+        server();
+        wait(NULL);
+    }
+}
+
 int main() {
     byteorder();
     test_hton_ntohl();
+    conn();
     return 0;
 }
